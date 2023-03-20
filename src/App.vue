72500d0beb7a76b8769fae3040184109fcc1b7b6
MarkAlexI <markovaleksandr108@gmail.com>
2023-03-20 00:08:34 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import HomePage from './views/HomePage.vue';
+</script>
+
+<template>
+  <div>
+    This is app.
+    <HomePage />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
