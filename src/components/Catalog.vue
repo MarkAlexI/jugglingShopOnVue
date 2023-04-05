eb84e8e3731e293c2ce8710fd4daddd3a5fbc002
MarkAlexI <markovaleksandr108@gmail.com>
2023-04-05 07:07:19 +0000

    Added options to Select.

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -6,10 +6,14 @@
       </div>
     </router-link>
     <h1>Catalog</h1>
-    <Select />
+    <Select
+      :selected="selected"
+      :options="categories.list"
+      @select="sortByCategories"
+    />
     <div class="catalog__list">
       <CatalogItem
-        v-for="product in products"
+        v-for="product in filteredProducts"
         :product_data="product"
         :key="product.article"
         @addToCart="addToCart(product)"
@@ -19,15 +23,48 @@
 </template>
 
 <script setup>
-  import { computed } from 'vue';
+  import { ref, reactive, computed } from 'vue';
   import { useShopStore } from '../stores/store.js';
   import CatalogItem from './CatalogItem.vue';
   import Select from './Select.vue';
+
+  const categories = reactive({
+      list: [
+        { name: 'All', value: 'all' },
+        { name: 'Knives', value: 'knives' },
+        { name: 'Kettle', value: 'kettle' },
+        { name: 'Ball', value: 'ball' },
+        { name: 'Rings', value: 'rings' }
+      ]
+  });
+
+  const selected = ref('All');
+  const sortedProducts = reactive({
+    list: []
+  });
 
   const store = useShopStore();
   const products = computed(() => store.products);
   const cart = computed(() => store.cart);
   const addToCart = computed(() => store.addToCart);
+
+  const sortByCategories = (category) => {
+    sortedProducts.list = [];
+    products.value.map((item) => {
+      if (item.category === category.name) {
+        sortedProducts.list.push(item);
+      }
+    });console.log(sortedProducts.list.length);
+    selected.value = category.name;
+  };
+
+  const filteredProducts = computed(() => {
+    if (sortedProducts.list.length > 0) {
+      return sortedProducts.list;
+    } else {
+      return products.value;
+    }
+  });
 </script>
 
 <style>
